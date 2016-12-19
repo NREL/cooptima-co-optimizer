833371638b4c253283ea58d78bf7022402360b1d
Grout <ray.grout@nrel.gov>
2016-12-19 11:42:47 -0700

    style

--- V0.1_RC1/property_mapping.py
+++ V0.1_RC1/property_mapping.py
@@ -2,91 +2,110 @@
 # the properties in the database
 
 import sys
-def check_data(dbprops,prop):
+
+
+def check_data(dbprops, prop):
     for p in prop:
         if not dbprops[p]:
-            sys.stderr.write("Data missing: {} for {}".format(p,dbprops['concat_name_cas2']))
+            sys.stderr.write("Data missing: {} for {}".format(p,
+                             dbprops['concat_name_cas2']))
             return False
     return True
 
+
 def RON(dbprops):
     if dbprops['___Identifier'] == 'Pure Compounds':
-        if not check_data(dbprops,['Pure_RON']):
+        if not check_data(dbprops, ['Pure_RON']):
             return 0.0
         return float(dbprops['Pure_RON'])
     elif dbprops['___Identifier'] == 'Fuel Blends':
-        if not check_data(dbprops,['Blend_RON']):
+        if not check_data(dbprops, ['Blend_RON']):
             return 0.0
         return float(dbprops['Blend_RON'])
 
+
 def LHV(dbprops):
     if dbprops['___Identifier'] == 'Pure Compounds':
-        if not check_data(dbprops,['Pure_LHV']):
+        if not check_data(dbprops, ['Pure_LHV']):
             return 0.0
         return float(dbprops['Pure_LHV'])
     elif dbprops['___Identifier'] == 'Fuel Blends':
-        if not check_data(dbprops,['Blend_LHV']):
+        if not check_data(dbprops, ['Blend_LHV']):
             return 0.0
         return float(dbprops['Blend_LHV'])
+
 
 def K(dbprops):
     return 0.0
 
+
 def sensitivity(dbprops):
     if dbprops['___Identifier'] == 'Pure Compounds':
-        if not check_data(dbprops,['Pure_RON','Pure_MON']):
+        if not check_data(dbprops, ['Pure_RON', 'Pure_MON']):
             return 0.0
         return float(dbprops['Pure_RON']) - float(dbprops['Pure_MON'])
     elif dbprops['___Identifier'] == 'Fuel Blends':
-        if not check_data(dbprops,['Blend_RON','Blend_MON']):
+        if not check_data(dbprops, ['Blend_RON', 'Blend_MON']):
             return 0.0
         return float(dbprops['Blend_RON']) - float(dbprops['Blend_MON'])
 
+
 def HoV(dbprops):
     if dbprops['___Identifier'] == 'Pure Compounds':
-        if not check_data(dbprops,['Pure_Heat_of_Vaporization',
-                            'Pure_Molecular_Weight']):
+        if not check_data(dbprops, ['Pure_Heat_of_Vaporization',
+                                    'Pure_Molecular_Weight']):
             return 0.0
-        return\
-             float(dbprops['Pure_Heat_of_Vaporization'])\
-             /float(dbprops['Pure_Molecular_Weight'])*1000.0
-    
+        return \
+            float(dbprops['Pure_Heat_of_Vaporization'])\
+          / float(dbprops['Pure_Molecular_Weight'])*1000.0
+
     elif dbprops['___Identifier'] == 'Fuel Blends':
-        if not check_data(dbprops,['Blend_Heat_of_Vaporization',
-                                  'Blend_Molecular_Weight']):
+        if not check_data(dbprops, ['Blend_Heat_of_Vaporization',
+                          'Blend_Molecular_Weight']):
             return 0.0
-        return float(dbprops['Blend_HoV_temp'])\
-                / float(dbprops['Blend_Molecular_Weight'])*1000.0
+        return float(dbprops['Blend_HoV_temp']) \
+             / float(dbprops['Blend_Molecular_Weight'])*1000.0
+
 
 def laminar_flame_speed(dbprops):
     return 0.0
 
+
 def lfv150(dbprops):
     return 0.0
+
 
 def AKI(dbprops):
     return 0.0
 
+
 def PMI(dbprops):
     return 0.0
+
 
 def getname(dbprops):
     return dbprops['concat_name_cas2']
 
+
 def formula(dbprops):
     return dbprops['Pure_Molecular_Formula']
+
 
 def molwt(dbprops):
     return float(dbprops['Pure_Molecular_Weight'])*1000.0
 
+
 def bp(dbprops):
     return float(dbprops['Pure_Boiling_Point'])
+
 
 def hof_liq(dbprops):
     return 0.0
 
+
 def lhv(dbprops):
     return dbprops['Pure_LHV']
+
 
 def prop_mapping(prop, propDB):
     prop_conv = {
@@ -103,10 +122,8 @@ def prop_mapping(prop, propDB):
         'FORMULA': formula,
         'MOLWT': molwt,
         'BP': bp,
-        'HoF_liq':hof_liq,
-        'LHV':lhv,
+        'HoF_liq': hof_liq,
+        'LHV': lhv,
     }
     fconv = prop_conv.get(prop)
     return fconv(propDB)
-
-
