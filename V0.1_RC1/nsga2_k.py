0129ac7f5d9c6ad1f2c5c943bab50bfca7e78676
Grout <ray.grout@nrel.gov>
2016-12-19 16:46:02 -0700

    style

--- V0.1_RC1/nsga2_k.py
+++ V0.1_RC1/nsga2_k.py
@@ -15,11 +15,8 @@
 
 import array
 import random
-#import json
 
 import numpy
-
-#from math import sqrt
 
 from merit_functions import mmf_single
 from blend_functions import blend_linear_vec as blend
@@ -32,7 +29,8 @@ from deap import creator
 from deap import tools
 
 
-def fraction_constraint(individual): #compute the constraint function value (sum over all parameters = 1)
+def fraction_constraint(individual):
+    """Compute the constraint function value (sum over all parameters = 1)"""
     return sum(individual)
 
 
@@ -47,26 +45,38 @@ def eval_mo(individual, propvec, Kinp):
     this_PMI = blend(individual, propvec, 'PMI')
     cost_f = blend(individual, propvec, 'COST')
 
-    merit_f = mmf_single(RON=this_ron, S=this_s, ON=this_on, HoV=this_HoV,SL=this_SL, K=Kinp) #note change in mmf_single (dynamic def of K)
+    merit_f = mmf_single(RON=this_ron, S=this_s, ON=this_on,
+                         HoV=this_HoV, SL=this_SL, K=Kinp)
+
     g_val = fraction_constraint(individual)
 
-    if (numpy.abs(g_val-1) <=1.0e-3):
-        penalty_mmf =0
-        penalty_cost=0
-    else: #theoretically, this part of the condition will never be entered
+    if (numpy.abs(g_val-1) <= 1.0e-3):
+        penalty_mmf = 0
+        penalty_cost = 0
+    else:  # theoretically, this part of the condition will never be entered
         raise 'Your parameters dont add up to 1 '
 
-    c_p_p = cost_f+penalty_cost #penalty_cost = 0 if nothing is changed about creation of individuals
-    mmf_p_p = -(merit_f+penalty_mmf) #penalty_mmf = 0 if nothing is changed about creation of individuals
+    # penalty_cost = 0 if nothing is changed about creation of individuals
+    c_p_p = cost_f+penalty_cost
+
+    # penalty_mmf = 0 if nothing is changed about creation of individuals
+    mmf_p_p = -(merit_f+penalty_mmf)
 
     return mmf_p_p, c_p_p
 
-    
-def uniform(low, up, size=None): #generate uniform random numbrs in [0,1] and scale so that sum = 1
-    X=[random.uniform(a, b) for a, b in zip([low] * size, [up] * size)] #uniformly generate new points within parameter bounds
-    sumX=sum(X) #sum up over all parameters in each individual
-    Y=[X[ii]/sumX for ii in range(len(X))] #divide each individual's parameters by the sum to satisfy sum x = 1 constraint;
-    return Y 
+
+def uniform(low, up, size=None):
+    """generate uniform random numbrs in [0,1] and scale so that sum = 1"""
+
+    # Uniformly generate new points within parameter bounds
+    X = [random.uniform(a, b) for a, b in zip([low] * size, [up] * size)]
+
+    sumX = sum(X)  # sum up over all parameters in each individual
+
+    # Divide each individual's parameters by the sum to satisfy
+    # sum x = 1 constraint;
+    Y = [X[ii] / sumX for ii in range(len(X))]
+    return Y
 
 
 def scale():
@@ -77,71 +87,75 @@ def scale():
                 sumChild = sum(child)
 
                 for i in xrange(len(child)):
-                    child[i]=child[i]/sumChild
-            
+                    child[i] = child[i]/sumChild
+
             return offspring
         return wrapper
     return decorator
 
 
-def nsga2_pareto_K(KK, propvec,seed=None):
+def nsga2_pareto_K(KK, propvec, seed=None):
 
-    NDIM = len(propvec['COST']) #number of parameters
+    NDIM = len(propvec['COST'])  # Number of parameters
 
-    creator.create("FitnessMin", base.Fitness, weights=(-1.0, -1.0)) #minimize both objectives (min -f(x) if maximization is needed)
-    creator.create("Individual", array.array, typecode='d', fitness=creator.FitnessMin) #individuals in the generation
+    # Minimize both objectives (min -f(x) if maximization is needed)
+    creator.create("FitnessMin", base.Fitness, weights=(-1.0, -1.0))
+
+    # Individuals in the generation
+    creator.create("Individual", array.array, typecode='d',
+                   fitness=creator.FitnessMin)
 
     toolbox = base.Toolbox()
-   
-    BOUND_LOW, BOUND_UP = 0.0, 1.0 #lower and upper variable bounds 
 
+    BOUND_LOW, BOUND_UP = 0.0, 1.0  # Lower and upper variable bounds
 
     toolbox.register("attr_float", uniform, BOUND_LOW, BOUND_UP, NDIM)
-    toolbox.register("individual", tools.initIterate, creator.Individual, toolbox.attr_float)
+    toolbox.register("individual", tools.initIterate, creator.Individual,
+                     toolbox.attr_float)
     toolbox.register("population", tools.initRepeat, list, toolbox.individual)
 
-#    toolbox.register("evaluate", eval_mo, Roninp=RONVEC,Sinp=SVEC, Oninp=ONVEC, HoVinp=HoVVEC, SLinp=SLVEC, LFinp=LFV150VEC, PMinp=PMIVEC, Costinp=COSTVEC,Kinp=KK)
-    toolbox.register("evaluate", eval_mo, propvec=propvec,Kinp=KK)
+    toolbox.register("evaluate", eval_mo, propvec=propvec, Kinp=KK)
 
-    toolbox.register("mate", tools.cxSimulatedBinaryBounded, low=BOUND_LOW, up=BOUND_UP, eta=20.0)
-    toolbox.register("mutate", tools.mutPolynomialBounded, low=BOUND_LOW, up=BOUND_UP, eta=20.0, indpb=1.0/NDIM)
+    toolbox.register("mate", tools.cxSimulatedBinaryBounded,
+                     low=BOUND_LOW, up=BOUND_UP, eta=20.0)
+    toolbox.register("mutate", tools.mutPolynomialBounded, low=BOUND_LOW,
+                     up=BOUND_UP, eta=20.0, indpb=1.0/NDIM)
 
     toolbox.decorate("mate", scale())
-    toolbox.decorate("mutate",scale())
+    toolbox.decorate("mutate", scale())
 
     toolbox.register("select", tools.selNSGA2)
 
-
-    #these are parameters that can be adjusted and may change the algorithm's performance 
-    NGEN = 200 #number of generations 
-    MU = 100 #number of individuals
-    CXPB = 0.75 #cross-over probability, [0,1]
+    #  These are parameters that can be adjusted and may change
+    #  the algorithm's performance
+    NGEN = 200  # Number of generations
+    MU = 100  # Number of individuals
+    CXPB = 0.75  # Cross-over probability, [0,1]
 
     stats = tools.Statistics(lambda ind: ind.fitness.values)
     stats.register("avg", numpy.mean, axis=0)
     stats.register("std", numpy.std, axis=0)
     stats.register("min", numpy.min, axis=0)
     stats.register("max", numpy.max, axis=0)
-    
+
     pf = tools.ParetoFront()
     hof = tools.HallOfFame(50)
     logbook = tools.Logbook()
     logbook.header = "gen", "evals", "std", "min", "avg", "max"
-    
+
     pop = toolbox.population(n=MU)
-    
-    
+
     # Evaluate the individuals with an invalid fitness
     invalid_ind = [ind for ind in pop if not ind.fitness.valid]
     fitnesses = toolbox.map(toolbox.evaluate, invalid_ind)
     for ind, fit in zip(invalid_ind, fitnesses):
         ind.fitness.values = fit
     pf.update(pop)
-    
+
     # This is just to assign the crowding distance to the individuals
     # no actual selection is done
     pop = toolbox.select(pop, len(pop))
-    
+
     record = stats.compile(pop)
     logbook.record(gen=0, evals=len(invalid_ind), **record)
     print(logbook.stream)
@@ -152,31 +166,30 @@ def nsga2_pareto_K(KK, propvec,seed=None):
         offspring = tools.selNSGA2(pop, len(pop))
         offspring = tools.selTournamentDCD(pop, len(pop))
         offspring = [toolbox.clone(ind) for ind in offspring]
-        
-        
+
         for ind1, ind2 in zip(offspring[::2], offspring[1::2]):
             if random.random() <= CXPB:
                 toolbox.mate(ind1, ind2)
-            
+
             toolbox.mutate(ind1)
             toolbox.mutate(ind2)
             del ind1.fitness.values, ind2.fitness.values
-        
+
         # Evaluate the individuals with an invalid fitness
         invalid_ind = [ind for ind in offspring if not ind.fitness.valid]
         fitnesses = toolbox.map(toolbox.evaluate, invalid_ind)
         for ind, fit in zip(invalid_ind, fitnesses):
-            ind.fitness.values = fit    
-        pf.update(offspring)   
+            ind.fitness.values = fit
+        pf.update(offspring)
+
         # Select the next generation population
         pop = toolbox.select(pop + offspring, MU)
         record = stats.compile(pop)
         logbook.record(gen=gen, evals=len(invalid_ind), **record)
         print(logbook.stream)
 
-    pop.sort(key=lambda x: x.fitness.values)    
+    pop.sort(key=lambda x: x.fitness.values)
     front = numpy.array([ind.fitness.values for ind in pop])
-    return front[:,1], -front[:,0]
+    return front[:, 1], -front[:, 0]
 
 #    return pop, logbook, hof, pf
-  
